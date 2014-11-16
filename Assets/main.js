595fb04fcfc885e316ecce85fc47ead441647bf9
Chelsea Thrupp <chelseathrupp@chelseas-mbp.gateway.pace.net>
2014-11-17 10:34:21 +1300

    Desktop Almost complete, final touches to be added

--- Assets/main.js
+++ Assets/main.js
@@ -1,7 +1,13 @@
 $(function(){
 
-	$(".mainSearch #search")on("click",function(){
-		$(".mainSearch #search").addClass("expand");
-	})
+$("#mainNav").stick_in_parent()
 
-}
+$("#mainNav").stick_in_parent()
+  .on("sticky_kit:stick", function(e) {
+    $("#mainNav").css("background-color", "red");
+  })
+  .on("sticky_kit:unstick", function(e) {
+    console.log("has unstuck!", e.target);
+  });
+
+});
